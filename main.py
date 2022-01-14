6898bbcd527733e52d6d000e2a29620c22898c74
NicholasWang <nicholas.x.wang@gmail.com>
2022-01-14 07:00:20 -0800

    more flask stuff

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask('app')
 @app.route("/")
 def home():
-    return "Hello World"
+    return render_template("index.html")
+@app.route("/njhs")
+def njhs():
+    return render_template("njhs.html")
+@app.route("/hour-tracker")
+def hour_tracker():
+    return render_template("hourtracker.html")
+@app.route("/njhs/hour-tracker")
+def njhs_hour_tracker():
+    return render_template("njhshourtracker.html")
 app.run(host='0.0.0.0', port=8080)
